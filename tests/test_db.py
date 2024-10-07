2c8304b85b57cefdb6767b30fb592036c325e089
Gabriel D'avila Pedro <gabrieldavilapedroo@gmail.com>
2024-10-07 10:10:59 -0300

    test: more database operation tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,11 +7,15 @@ def test_author_table_is_healthy():
 
     Author.objects.create(name="Andrea de souza")
     number_of_authors = Author.objects.all()
+    lest_author = Author.objects.last()
     assert number_of_authors.count() == 4
+    assert lest_author.name == "Andrea de souza"
 
     Author.objects.get(id=4).delete()
     number_of_authors = Author.objects.all()
+    lest_author = Author.objects.last()
     assert number_of_authors.count() == 3
+    assert lest_author.name != "Andrea de souza"
 
 
 def test_book_table_is_healthy():
@@ -21,7 +25,11 @@ def test_book_table_is_healthy():
     Book.objects.create(name="C++", edition=3, publication_year=2021)
     number_of_books = Book.objects.all()
     assert number_of_books.count() == 7
+    lest_book = Book.objects.last()
+    assert lest_book.name == "C++"
 
     Book.objects.get(id=7).delete()
+    lest_book = Book.objects.last()
     number_of_books = Book.objects.all()
     assert number_of_books.count() == 6
+    assert lest_book.name != "C++"
